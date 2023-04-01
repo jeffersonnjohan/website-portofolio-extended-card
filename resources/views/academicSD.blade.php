198172d625aa34864f28102f314ec768c41bef7e
jeffersonnjohan <jeffersonnjohan@gmail.com>
2023-04-02 00:51:38 +0700

    Primary School Page Done

--- resources/views/academicSD.blade.php
+++ resources/views/academicSD.blade.php
@@ -2,8 +2,10 @@
 
 @section('contentAcademic')
 <h2 class="text-5xl underline mb-16 text-center">Primary School</h2>
-<img src="https://source.unsplash.com/600x300?background-pink" alt="Image 1" class = "w-3/5 m-auto mb-8">
-<p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sequi obcaecati quam vel porro consequatur dolorem ullam enim velit repudiandae quis repellendus modi aspernatur quibusdam illum, sapiente asperiores tenetur voluptates!</p>
-<img src="https://source.unsplash.com/600x300?background-pink" alt="Image 1" class = "w-3/5 m-auto mb-8">
-<p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sequi obcaecati quam vel porro consequatur dolorem ullam enim velit repudiandae quis repellendus modi aspernatur quibusdam illum, sapiente asperiores tenetur voluptates!</p>
+<img src="img/fotoSD.jpg" alt="Image 1" class = "w-3/5 m-auto mb-8">
+<p class="mb-4">Can you guess which one i'm? Hahahaha, maybe it's hard. Because my face is different now. When i was on Primary School, i was naughty and ignorant. I often got punished by my teacher whenever i made mistake.</p>
+<p class="mb-4">Every day every night, i was always fraid to go to school tommorow morning. Many of my teacher is terrifying :D. But i believe that they shaped who i am now.</p>
+<img src="img/hasilUN.jpg" alt="Image 1" class = "w-3/5 m-auto mb-8">
+<p class="mb-4">The peak of my primary school life is i got first rank of national test. I wasn't ever expect to get this title. In my class, i never got 5 best ranking. But at the end, i was surprised that my national score is biggest at my school.</p>
+<p class="mb-4">After the announcement, my mom was very proud of me. She often told this to my neighbor, and i felt that i'm the smartest people in my neighborhood hahahha. Note: That's all is only my feeling, not the fact.</p>
 @endsection
