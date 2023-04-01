5ff2b40932450cbfd16ee76f58bf053a403872ab
jeffersonnjohan <jeffersonnjohan@gmail.com>
2023-04-01 22:35:55 +0700

    Fixing Bug Relative Url CSS

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&family=Poppins:ital,wght@0,200;0,400;1,600;1,700&display=swap" rel="stylesheet">
